21dd0fa4e12df13414f19678aa2a2a5721332575
Rohit Rana <rrana425@outlook.com>
2024-12-30 05:23:47 +0000

    Editing project web frontend writing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const requestHandler = (req, res) => {
-    res.end('Hello, Kubernetes!');
+    res.end('Hello, Kubernetes!, I am a test project based on nodeJS');
 };
 
 const server = http.createServer(requestHandler);
